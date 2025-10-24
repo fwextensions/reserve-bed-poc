0e4bc37f885a1df8d0a80395e9e25a439ebc0b60
John Dunning <fw@johndunning.com>
2025-10-24 12:14:43 -0700

    Include convex/_generated/ files in repo
    Update package.json build scripts so convex deploy runs a build first.

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,53 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as beds from "../beds.js";
+import type * as crons from "../crons.js";
+import type * as holds from "../holds.js";
+import type * as reservations from "../reservations.js";
+import type * as seed from "../seed.js";
+import type * as sites from "../sites.js";
+import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  beds: typeof beds;
+  crons: typeof crons;
+  holds: typeof holds;
+  reservations: typeof reservations;
+  seed: typeof seed;
+  sites: typeof sites;
+  users: typeof users;
+}>;
+declare const fullApiWithMounts: typeof fullApi;
+
+export declare const api: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
